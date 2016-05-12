2aa8b2218faf8ef67b0e347f338dbf4776180a3c
Denys <cossack1996@gmail.com>
2016-05-13 00:55:49 +0300

    Fixed all bugs

--- js/script.js
+++ js/script.js
@@ -43,24 +43,31 @@
             length: [[16]],
             code: [[3528, 3589]]
         }
-    ];
+    ],
+    IS_VALID = "Is valid",
+    IS_INVALID = "Is invalid card number",
+    NO_MORE_CARDS = "No more card numbers available for this vendor",
+    UNKNOWN_VENDOR = "Unknown";
     
     inputEment.addEventListener("input", function () {
-        var newValue = this.value.replace(/[^0-9.]/g, '').replace(/(.{4})/g, '$1 ').trim(),
+        var valueWithoutLatters = this.value.replace(/[^0-9]/g, ''),
             valueWithoutSpaces = this.value.replace(/ /g, '');
         
-        if (newValue != valueWithoutSpaces) {
-            this.value = newValue;
+        if (valueWithoutLatters != valueWithoutSpaces) {
             $.setError(this);
+        } else {
+            $.removeError(this);
         }
+        
+        this.value = valueWithoutLatters.replace(/(.{4})/g, '$1 ').trim()
     });
     
-    function luhnAlgorithm (value) {
+    function isValidCard(value) {
         var sum = 0;
         
-        for (var i = 1; i < value.length; i++) {
-            var number = value[value.length - i];
-            
+        for (var i = 1; i <= value.length; i++) {
+            var number = parseInt(value[value.length - i]);
+
             if (i % 2 == 0) {
                 number = number * 2;
                 
@@ -71,62 +78,65 @@
 
             sum = sum + number;
         }
-        
+                
         sum = 10 - (sum % 10);
         
         if (sum == 10) {
             sum = 0;
         }
+
+        return !sum;
+    }
+    
+    function getVendor(value) {
+        var resultVendor = {
+            name: UNKNOWN_VENDOR
+        };
         
-        return sum;
+        if (isValidCard(value)) {
+            for (var i = 0; i < VENDORS.length; i++) {
+                var currentVendor = VENDORS[i];
+                var resultOfCicle = {
+                    length: false,
+                    code: false
+                }
+
+                for (var j = 0; j < currentVendor.length.length; j++) {
+                    var valueLength = value.length;
+
+                    if (valueLength >= currentVendor.length[j][0] && valueLength <= (currentVendor.length[j][1] || currentVendor.length[j][0])) {
+                        resultOfCicle.length = true;
+                    }
+                }
+
+                for (var j = 0; j < currentVendor.code.length; j++) {
+                    var valueCode = parseInt(value.substring(0, currentVendor.code[j][0].toString().length));
+
+                    if (valueCode >= currentVendor.code[j][0] && valueCode <= (currentVendor.code[j][1] || currentVendor.code[j][0])) {
+                        resultOfCicle.code = true;
+                    }
+                }
+
+                if (resultOfCicle.code && resultOfCicle.length) {
+                    resultVendor = currentVendor;
+                    break;
+                }
+            }
+        }
+        
+        return resultVendor;
     }
     
     function GetCreditCardVendor() {
         var value = inputEment.value.replace(/[^0-9.]/g, '');
-        var resultVendor = {
-            name: "Unknown"
-        };
         
-        for (var i = 0; i < VENDORS.length; i++) {
-            var currentVendor = VENDORS[i];
-            var resultOfCicle = {
-                length: false,
-                code: false
-            }
-            
-            for (var j = 0; j < currentVendor.length.length; j++) {
-                var valueLength = value.length;
-                
-                if (valueLength >= currentVendor.length[j][0] && valueLength <= (currentVendor.length[j][1] || currentVendor.length[j][0])) {
-                    resultOfCicle.length = true;
-                }
-            }
-            
-            for (var j = 0; j < currentVendor.code.length; j++) {
-                var valueCode = parseInt(value.substring(0, currentVendor.code[j][0].toString().length));
-
-                if (valueCode >= currentVendor.code[j][0] && valueCode <= (currentVendor.code[j][1] || currentVendor.code[j][0])) {
-                    resultOfCicle.code = true;
-                }
-            }
-            
-            if (resultOfCicle.code && resultOfCicle.length) {
-                resultVendor = currentVendor;
-                break;
-            }
-        }
-        
-        resultElement.innerHTML = resultVendor.name;
+        resultElement.innerHTML = getVendor(value).name;
     }
 
     function IsCreditCardNumberValid() {
-        var value = inputEment.value.replace(/[^0-9.]/g, ''),
-            IS_VALID = "Is valid",
-            IS_INVALID = "Is invalid";
+        var value = inputEment.value.replace(/[^0-9.]/g, '');
         
-        var sum = luhnAlgorithm(value);
-        
-        if (value[value.length - 1] == sum) {
+        if (isValidCard(value)) {
             resultElement.innerHTML = IS_VALID;
         } else {
             resultElement.innerHTML = IS_INVALID;
@@ -135,13 +145,35 @@
 
     function GenerateNextCreditCardNumber() {
         var value = inputEment.value.replace(/[^0-9.]/g, '');
-        var newValue = parseInt(value) + 1;
+        var vendorName = getVendor(value).name;
+        
+        function res(a, b, result, carry, base) {
+            if(a.length == 0 && b.length == 0 && !carry)
+                return result;
 
-        while (newValue.toString()[newValue.toString().length - 1] != luhnAlgorithm(newValue.toString())) {
-            newValue++;
+            var left = parseInt(a.pop() || '0', 10);
+            var right = parseInt(b.pop() || '0', 10);
+
+            var l = left + right + (carry || 0);
+
+            return res(a, b, l % base + (result || ""), Math.floor(l/base), base);
+        }
+
+        function add(a, b) {
+            return res(a.toString().split(""), b.toString().split(""), "","",10).toString();
         }
         
-        resultElement.innerHTML = newValue;
+        value = add(value, 1);
+
+        while (!isValidCard(value)) {
+            value = add(value, 1);
+        }
+        
+        if ((vendorName != UNKNOWN_VENDOR) && (getVendor(value).name != vendorName)) {
+            value = NO_MORE_CARDS;
+        }
+        
+        resultElement.innerHTML = value;
     }
     
     window.GetCreditCardVendor = GetCreditCardVendor;
